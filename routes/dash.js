2516c963f01dd84d4969fcee908b542f73e17e42
HassanBahati <mukisabahati@gmail.com>
2021-03-25 21:42:21 +0300

    fix bug

--- routes/dash.js
+++ routes/dash.js
@@ -15,5 +15,20 @@ router.get('/humanResource', (req, res) => {
     res.render('humanResource')
 })
 
+//finance
+router.get('/finance', (req, res) => {
+    res.render('finance')
+});
+
+//trucks
+router.get('/trucks', (req, res) => {
+    res.render('trucks')
+})
+
+//orders
+router.get('/orders', (req, res) => {
+    res.render('orders')
+})
+
 //exporting router 
 module.exports = router;

ff73b54331a29883beacefbe7e4b31d21071737f
HassanBahati <mukisabahati@gmail.com>
2021-03-25 14:32:43 +0300

    changed all html files to pug

--- routes/dash.js
+++ routes/dash.js
@@ -8,6 +8,11 @@ const router = express.Router();
 //dashboard
 router.get('/', (req, res) => {
     res.render('dash')
+});
+
+//humanresource
+router.get('/humanResource', (req, res) => {
+    res.render('humanResource')
 })
 
 //exporting router 
